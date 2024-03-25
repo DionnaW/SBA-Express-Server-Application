cefd3f007fac8026e1a34bccb834cedea143782b
Dionna <businessladythick@yahoo.com>
2024-03-25 16:16:03 -0400

    idea generated for porject 318

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my Express Server Application')
-});
-
 app.get('/', (req, res) => {  //homepage
-    res.send('Welcome to my Express Server Application')
+    res.send('Welcome to Candy Land')
 });
 
 app.get('/about', (req, res) => {   //about section on website
-    res.send('Thanks for wanting to see what we are about')
+    res.send('Thanks for wanting to know more!')
 });
 
 app.listen(4000);
