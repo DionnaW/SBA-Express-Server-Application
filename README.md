114f01645f6c31292ed73c0fb6eb9ef45b09ce3d
Dionna <businessladythick@yahoo.com>
2024-03-26 15:31:38 -0400

    added .gitignore file too

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ i created all of my files, folders, and packages
 taking a break!
 i have a few routes running, but stuck, so back at it tomorrow
 
+3-26-24
+i added PUT and DELETE routes, but it's not running how it should at my local host
+created query parameters and have success
+stopping for now, and since i have 3 days i'm hoping to be finished tomorrow
+
--- main.js
+++ main.js
@@ -1,40 +1,108 @@
+//getting started with application
 const express = require('express');
 const app = express();
-const path = require('path')
 
-//global middleware that would apply 2 all routes = a function to access and modify a req and res objects
-app.use(express.urlencoded({extended: false})) 
-app.use(express.static(path.join(__dirname, "public")))
+//making a path so engine views can be added
+const path = require('path');
+
+//global middleware for all routes
+app.use(express.urlencoded({extended: false}));
+
+//custom middleware for buyCandy
+app.use(buyCandy);
+
+//custom middleware to link css
+app.use(express.static(path.join(__dirname, "public")));
+
+//adding ejs view engine file to this doc
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"));
 
-//route w/GET request     //parameters (req & res)
-app.get("/", (req, res) => {  //homepage
-    res.render("home")
-});
+//creating the buyCandy func
+function buyCandy(req, res, next) {
+  req.visitorShopping = true
+  if (req.buyCandy) {
+    res.send("So much to choose from, so take your time and look around")
+  } else {
+    next()
+  }
+}
 
-//route
-app.get('/about', (req, res) => {   //this will be for the about section on the page
-    res.send("Thanks for loving our candy!")
+//get request route for webpage
+app.get("/", (req, res) => {
+    res.render("home", {
+        isShopping: req.visitorShopping,
+        choices: [
+            {name: "Hard Candy", types: "all flavors"}, 
+            {name: "Soft Candy", types: "all flavors"}, 
+            {name: "Chewing Candy", types: "all flavors"}
+        ]
+    })
+})
+
+//get request route for /about section on webpage with exclusive candies
+app.get('/about', (req, res) => {  
+    const candyType = req.query.type;
+    const candies = [
+        {id: 1, name: "Chocolate Bar", tpye: "Chocolate"},
+        {id: 2, name: "Gummy Bears", type: "Gummy"},
+        {id: 3, name: "Lollipop", type: "Sucker"}
+    ];
+
+    const filteredCandies = candies.filter(candy => {
+        if (candyType) {
+            return candy.type === candyType;
+        } else {
+            return true;
+        }
+    });
+
+    //making filtered candies a response
+    res.json(filteredCandies);
 }); 
 
-//route w/POST request; to get a response url from a form submission that ask any question
-app.post("/result", (req, res) => {
-    if (req.body.day.trim().toUpperCase() === "EVERYDAY") {
-        res.send("YAY, that is a great answer.")
+//creating a delete route for a candy in the exclusive category (gummy bears) & use 204 for successful delete & 404 if not found
+app.delete("/candies/:id", (req, res) => {
+    const candyId = parseInt(req.params.id);
+
+    const candyIndex = candies.findIndex(candy => candy.id === candyId);
+
+    if (candyIndex !== -1) {
+        candies.splice(candyIndex, 1);
+        res.status(204).send();
     } else {
-        res.send("Incorrect, please try again")
+        res.status(404).json({error: "Candy not found"});
+    }
+});
+
+//creating a put route for the candy in the exclusive category, if it's found
+app.put("/candies/:id", (req, res) => {
+    const candyId = parseInt(req.params.id);
+
+    const candyIndex = candies.findIndex(candy => candy.id === candyId);
+
+    if (candyIndex !== -1) {
+        candies[candyIndex].name = req.body.name;
+        res.json(candies[candyIndex]);
+    } else {
+        res.status(404).json({error: "Candy not found"});
+    }
+});
+
+//post route for response from get request
+app.post("/result", (req, res) => {
+    if(req.body.day.trim().toUpperCase() === "EVERYDAY") {
+      res.send("Yay, that is the best answer!")
+    } else {
+      res.send("Wrong answer, try again!")
     }
 })
 
- //allows us access to what the user actually typed in to throw a true or false ststement
- //(trim removes any whitespace & upper keeps it ture if capital or lower case)
-
-//route w/get request; to get a response url from a form submission
+//get request for when localhost:4000/result is visited
 app.get("/result", (req, res) => {
-    res.send("You made it to the wrong page; go back?")
+    res.send("This page is invalid, go back!")
 })
 
-
-
 app.listen(4000);
+
+
